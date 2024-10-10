e75b72a44113dbed3c16b742fa278f66616c49aa
rahmanlana <rahmanlana044@gmail.com>
2024-10-10 08:44:13 +0700

    P6_Percobaan1_Peranyaan2_21

--- src/pertemuan7/SiakadFor21.java
+++ src/pertemuan7/SiakadFor21.java
@@ -10,7 +10,7 @@ public class SiakadFor21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 100, terendah = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
