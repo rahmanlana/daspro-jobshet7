1d44395be8e57a45e11b7260238180dedf9fcd43
rahmanlana <rahmanlana044@gmail.com>
2024-10-10 09:02:51 +0700

    P7_Percobaan1_PertanyaanNo5_21

--- src/pertemuan7/SiakadFor21.java
+++ src/pertemuan7/SiakadFor21.java
@@ -10,7 +10,8 @@ public class SiakadFor21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 100, terendah = 0;
+        double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
@@ -21,9 +22,16 @@ public class SiakadFor21 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai < 60) {
+                jumlahTidakLulus++;
+            } else {
+                jumlahLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah Lulus: " + jumlahLulus);
+        System.out.println("Jumlah Tidak Lulus: " + jumlahTidakLulus);
     }
 
 }
